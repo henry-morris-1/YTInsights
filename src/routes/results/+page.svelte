fe832958ed9f79198c8c21637b14bbb3362f9a6c
Henry Morris <hrmorri5@ncsu.edu>
2025-07-31 08:38:22 -0400

    use tailwind colors

--- src/routes/results/+page.svelte
+++ src/routes/results/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <ContainerCentered>
-    <div class="w-100 p-10 aspect-square rounded-full bg-conic-[yellow_0%,green_25%,transparent_25%,transparent_75%,red_75%,yellow]">
+    <div class="w-100 p-10 aspect-square rounded-full bg-conic-[theme(colors.yellow.500)_0%,theme(colors.green.500)_25%,transparent_25%,transparent_75%,theme(colors.red.500)_75%,theme(colors.yellow.500)]">
         <div class="w-full aspect-square rounded-full bg-neutral-900"></div>
     </div>
 </ContainerCentered>
