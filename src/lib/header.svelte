285dc35dfe7361084dca77f694fa07c672d27f6b
Henry Morris <hrmorri5@ncsu.edu>
2025-08-09 19:01:05 -0400

    Spinning coin in header

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -1,5 +1,15 @@
-<header class="z-1000 sticky top-0 left-0 right-0 flex gap-4 justify-center py-4 px-8 backdrop-blur-md">
-    <div></div>
-    <div></div>
-    <div></div>
+<script>
+    import Coin from "./sweets/coin.svelte"
+</script>
+
+<header class="z-1000 sticky top-0 left-0 right-0 h-25 p-8 flex gap-4 justify-center backdrop-blur-md">
+    <div>
+
+    </div>
+    <div>
+
+    </div>
+    <div class="h-full aspect-square">
+        <Coin />
+    </div>
 </header>
---
+++ src/lib/sweets/coin.svelte
@@ -0,0 +1,20 @@
+<style>
+    @property --angle {
+        syntax: '<angle>';
+        inherits: false;
+        initial-value: 0deg;
+    }
+    @keyframes rotate {
+        from { --angle: 90deg; }
+        to { --angle: 270deg; }
+    }
+    .animate-rotate {
+        animation: rotate 1s linear infinite;
+    }
+</style>
+
+<div class="w-full h-full aspect-square [container-type:size] flex items-center justify-center perspective-distant transform-3d">
+    <div class="absolute inset-0 bg-yellow-500 rounded-full animate-rotate [transform:translateX(calc(5cqw_*_sin(calc(var(--angle)_+_180deg))))_translateZ(calc(5cqw_*_cos(calc(var(--angle)_+_180deg))))_rotateY(calc(var(--angle)_+_180deg))]"></div>
+    <div class="absolute inset-0 bg-yellow-700 rounded-full animate-rotate [transform:translateX(calc(5cqw_*_sin(var(--angle))))_translateZ(calc(5cqw_*_cos(var(--angle))))_rotateY(var(--angle))]"></div>
+    <div class="absolute inset-y-0 w-[10cqh] bg-yellow-700 [transform:rotateY(var(--angle))]"></div>
+</div>
