f468d908f823f7889acf23f06b40a1b2b7ecbd87
Henry Morris <hrmorri5@ncsu.edu>
2025-12-19 21:44:27 -0500

    Removed sphere

--- src/lib/text/heading.svelte
+++ src/lib/text/heading.svelte
@@ -6,5 +6,5 @@
     <h2 class="flex items-end justify-between text-3xl font-bold">
         {@render children()}
     </h2>
-    <div class="h-0.5 w-full bg-neutral-300"></div>
+    <div class="h-0.5 w-full bg-medium"></div>
 </div>
